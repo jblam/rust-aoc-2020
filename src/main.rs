4dc82be4552ca932653a7dd944eae0ab2ec8f894
jblam <jeremy.blamey@gmail.com>
2021-01-01 16:34:19 +1100

    Day 5 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-fn main() {
-    let s = include_str!("data\\day4-input.txt");
-    println!("Part 1: {:?}", day4::part1(s));
-    println!("Part 2: {:?}", day4::part2(s));
-}
+mod day5;
 
-mod day4;
+fn main() {
+    let s = include_str!("data\\day5-input.txt");
+    println!("Part 1: {:?}", day5::part1(s));
+    println!("Part 2: {:?}", day5::part2(s));
+}
