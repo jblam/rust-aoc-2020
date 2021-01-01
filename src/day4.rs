e67ca362a0f2bf2da6ae4761a8fe90614eea49ec
jblam <jeremy.blamey@gmail.com>
2021-01-01 14:57:30 +1100

    Day 4, part 2

--- src/day4.rs
+++ src/day4.rs
@@ -6,10 +6,17 @@ use std::{
 };
 
 pub fn part1(s: &'static str) -> usize {
-    parse(s, "\n\n").filter(|r| r.as_ref().unwrap().is_valid()).count()
+    parse(s, "\n\n")
+        .filter(|r| r.as_ref().unwrap().is_valid())
+        .count()
 }
-pub fn part2(_: &str) {
-    todo!()
+pub fn part2(s: &'static str) -> usize {
+    parse(s, "\n\n")
+        .filter(|r| {
+            let record = r.as_ref().unwrap();
+            record.is_valid() && record.is_contents_valid()
+        })
+        .count()
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -77,6 +84,43 @@ impl FromStr for Record {
             .map(Record)
     }
 }
+impl Field {
+    fn is_valid(&self) -> bool {
+        fn is_rgb(s: &String) -> bool {
+            let b = s.as_bytes();
+            b[0] == b'#'
+                && b[1..].len() == 6
+                && b[1..].iter().all(|b| (*b as char).is_ascii_hexdigit())
+        }
+        fn is_eyecolour(s: &String) -> bool {
+            match s.as_str() {
+                "amb" => true,
+                "blu" => true,
+                "brn" => true,
+                "gry" => true,
+                "grn" => true,
+                "hzl" => true,
+                "oth" => true,
+                _ => false,
+            }
+        }
+        fn is_passport_id(s: &String) -> bool {
+            s.len() == 9 && s.chars().all(|c| c.is_ascii_digit())
+        }
+        match self {
+            Field::BirthYear(year) => *year >= 1920 && *year <= 2002,
+            Field::IssueYear(year) => *year >= 2010 && *year <= 2020,
+            Field::ExpirationYear(year) => *year >= 2020 && *year <= 2030,
+            Field::Height(Length::Centimetre(cm)) => *cm >= 150 && *cm <= 193,
+            Field::Height(Length::Inch(inch)) => *inch >= 59 && *inch <= 76,
+            Field::Height(_) => false,
+            Field::HairColour(s) => is_rgb(&s),
+            Field::EyeColour(s) => is_eyecolour(&s),
+            Field::PassportId(s) => is_passport_id(&s),
+            Field::CountryId(_) => true,
+        }
+    }
+}
 impl Record {
     fn is_valid(&self) -> bool {
         let all_fields = self.0.iter().map(discriminant).collect::<HashSet<_>>();
@@ -88,6 +132,9 @@ impl Record {
             && (all_fields.len() == FIELD_KIND_COUNT // has all the fields, or
                 // is missing only the optional field
                 || (all_fields.len() == FIELD_KIND_COUNT - 1 && !all_fields.contains(&optional_field)))
+    }
+    fn is_contents_valid(&self) -> bool {
+        self.0.iter().all(Field::is_valid)
     }
 }
 fn parse(s: &'static str, double_newline: &'static str) -> impl Iterator<Item = Result<Record>> {
@@ -168,31 +215,32 @@ iyr:2011 ecl:brn hgt:59in";
     fn record_invalid_with_missing_fields() {
         assert!(!Record(Default::default()).is_valid())
     }
-    impl Record {}
-    fn make_minimal_record() -> Record {
-        Record(vec![
-            Field::BirthYear(0),
-            Field::IssueYear(0),
-            Field::ExpirationYear(0),
-            Field::Height(Length::Centimetre(0)),
-            Field::HairColour(Default::default()),
-            Field::EyeColour(Default::default()),
-            Field::PassportId(Default::default()),
-        ])
+    impl Record {
+        fn minimal() -> Record {
+            Record(vec![
+                Field::BirthYear(0),
+                Field::IssueYear(0),
+                Field::ExpirationYear(0),
+                Field::Height(Length::Centimetre(0)),
+                Field::HairColour(Default::default()),
+                Field::EyeColour(Default::default()),
+                Field::PassportId(Default::default()),
+            ])
+        }
     }
     #[test]
     fn record_valid_with_only_required_fields() {
-        assert!(make_minimal_record().is_valid())
+        assert!(Record::minimal().is_valid())
     }
     #[test]
     fn record_valid_with_all_fields() {
-        let mut r = make_minimal_record();
+        let mut r = Record::minimal();
         r.0.push(Field::CountryId(0));
         assert!(r.is_valid())
     }
     #[test]
     fn record_invalid_with_duplicate() {
-        let mut r = make_minimal_record();
+        let mut r = Record::minimal();
         r.0.push(Field::BirthYear(0));
         assert!(!r.is_valid())
     }
@@ -200,5 +248,46 @@ iyr:2011 ecl:brn hgt:59in";
     #[test]
     fn validates_example_part1() {
         assert_eq!(2, part1(EXAMPLE));
+    }
+
+    #[test]
+    fn invalidates_example_part2() {
+        const EXAMPLE: &str = "eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007";
+        assert_eq!(
+            4,
+            parse(EXAMPLE, "\n\n")
+                .flatten()
+                .filter(|r| r.is_valid() && !r.is_contents_valid())
+                .count()
+        )
+    }
+
+    #[test]
+    fn validates_example_part2() {
+        const EXAMPLE: &str = "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719";
+        assert_eq!(4, part2(EXAMPLE))
     }
 }
