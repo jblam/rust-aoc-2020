389cd0193308ec8d47dcc367a2fe7152891247c7
jblam <jeremy.blamey@gmail.com>
2021-01-01 17:44:27 +1100

    Day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -1,11 +1,13 @@
 use std::collections::HashSet;
 
 pub fn part1(s: &str) -> usize {
-    to_groups(s).map(Result::unwrap).sum()
+    to_union_counts(s).map(Result::unwrap).sum()
 }
-pub fn part2(_: &str) {}
+pub fn part2(s: &str) -> usize {
+    to_intersection_counts(s).sum()
+}
 
-fn to_groups(s: &str) -> impl Iterator<Item = Result<usize, String>> + '_ {
+fn to_union_counts(s: &str) -> impl Iterator<Item = Result<usize, String>> + '_ {
     s.split("\n\n").map(|t| {
         t.bytes()
             .filter(|b| !b.is_ascii_whitespace())
@@ -18,6 +20,22 @@ fn to_groups(s: &str) -> impl Iterator<Item = Result<usize, String>> + '_ {
             })
             .collect::<Result<HashSet<_>, String>>()
             .map(|s| s.len())
+    })
+}
+
+fn to_intersection_counts(s: &str) -> impl Iterator<Item = usize> + '_ {
+    s.split("\n\n").map(|t| {
+        let line_bytes = t.lines().map(|l| l.bytes().collect::<HashSet<_>>());
+        let mut intersect: Option<HashSet<u8>> = None;
+        for set in line_bytes {
+            intersect = Some(if let Some(mut prev) = intersect {
+                prev.retain(|e| set.contains(e));
+                prev
+            } else {
+                set
+            })
+        }
+        intersect.map_or(0, |s| s.len())
     })
 }
 
@@ -41,14 +59,28 @@ a
 
 b";
     #[test]
-    fn gets_groups() {
+    fn gets_union_counts() {
         assert_eq!(
             vec![3, 3, 3, 1, 1],
-            to_groups(EXAMPLE).collect::<Result<Vec<_>, _>>().unwrap()
+            to_union_counts(EXAMPLE)
+                .collect::<Result<Vec<_>, _>>()
+                .unwrap()
         )
     }
     #[test]
     fn gets_example_part1() {
         assert_eq!(11, part1(EXAMPLE));
+    }
+
+    #[test]
+    fn gets_intersect_counts() {
+        assert_eq!(
+            vec![3, 0, 1, 1, 1],
+            to_intersection_counts(EXAMPLE).collect::<Vec<_>>()
+        )
+    }
+    #[test]
+    fn gets_example_part2() {
+        assert_eq!(6, part2(EXAMPLE))
     }
 }
