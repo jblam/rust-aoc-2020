1c525c779b950fd331b181947c668eb81ce088b0
jblam <jeremy.blamey@gmail.com>
2021-01-01 14:14:42 +1100

    validation of records

--- src/day4.rs
+++ src/day4.rs
@@ -1,5 +1,9 @@
 use anyhow::{bail, Error, Result};
-use std::str::FromStr;
+use std::{
+    collections::HashSet,
+    mem::{discriminant, Discriminant},
+    str::FromStr,
+};
 
 pub fn part1(_: &str) {
     todo!()
@@ -25,6 +29,7 @@ enum Field {
     CountryId(i32),
 }
 
+#[derive(Debug)]
 struct Record(Vec<Field>);
 
 impl FromStr for Length {
@@ -64,17 +69,36 @@ impl FromStr for Field {
 impl FromStr for Record {
     type Err = Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        s.split_whitespace().map(Field::from_str).collect::<Result<Vec<_>>>().map(Record)
+        s.split_whitespace()
+            .map(Field::from_str)
+            .collect::<Result<Vec<_>>>()
+            .map(Record)
     }
 }
-fn parse(s: &'static str) -> impl Iterator<Item = Result<Record>> {
-    const PATTERN: &str = "\n\n";
-    s.split(PATTERN)
-        .map(str::parse)
+impl Record {
+    fn is_valid(&self) -> bool {
+        let all_fields = self.0.iter().map(discriminant).collect::<HashSet<_>>();
+        const FIELD_KIND_COUNT: usize = 8;
+        // JB 2021-01-01: morally const, but std::mem::discriminant is not const fn.
+        let optional_field: Discriminant<Field> =
+            discriminant(&Field::CountryId(Default::default()));
+        (all_fields.len() == self.0.len()) // duplicate field kinds not allowed
+            && (all_fields.len() == FIELD_KIND_COUNT // has all the fields, or
+                // is missing only the optional field
+                || (all_fields.len() == FIELD_KIND_COUNT - 1 && !all_fields.contains(&optional_field)))
+    }
+}
+fn parse(s: &'static str, double_newline: &'static str) -> impl Iterator<Item = Result<Record>> {
+    s.split(double_newline).map(str::parse)
 }
 
 #[cfg(test)]
 mod tests {
+    use std::{
+        collections::HashSet,
+        mem::{discriminant, Discriminant},
+    };
+
     use super::*;
 
     #[test]
@@ -83,10 +107,91 @@ mod tests {
     }
     #[test]
     fn can_parse_string_field() {
-        assert_eq!(Field::HairColour("Gross".to_string()), "hcl:Gross".parse().unwrap())
+        assert_eq!(
+            Field::HairColour("Gross".to_string()),
+            "hcl:Gross".parse().unwrap()
+        )
     }
     #[test]
     fn can_parse_length_field() {
-        assert_eq!(Field::Height(Length::Centimetre(123)), "hgt:123cm".parse().unwrap())
+        assert_eq!(
+            Field::Height(Length::Centimetre(123)),
+            "hgt:123cm".parse().unwrap()
+        )
+    }
+    #[test]
+    fn can_parse_record_line() {
+        assert!(vec![Field::BirthYear(1), Field::IssueYear(1)]
+            .iter()
+            .eq("byr:1 iyr:1".parse::<Record>().unwrap().0.iter()))
+    }
+    #[test]
+    fn can_parse_record_multiline() {
+        assert!(vec![Field::BirthYear(1), Field::IssueYear(1)]
+            .iter()
+            .eq("byr:1\niyr:1".parse::<Record>().unwrap().0.iter()))
+    }
+
+    const EXAMPLE: &str = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in";
+
+    #[test]
+    fn can_parse_records() {
+        // JB 2021-01-01: unclear why Rust is not finding any CRs in the above string.
+        // I'm pretty sure there's CRs in the *source*.
+        let result = parse(EXAMPLE, "\n\n").collect::<Result<Vec<_>>>();
+        assert_eq!(result.unwrap().len(), 4)
+    }
+    #[test]
+    fn can_interpret_discriminant() {
+        let mut found_records: HashSet<Discriminant<Field>> = Default::default();
+        let record = Record(vec![Field::BirthYear(1), Field::IssueYear(2)]);
+        for f in record.0 {
+            assert!(found_records.insert(discriminant(&f)))
+        }
+        assert!(!found_records.insert(discriminant(&Field::BirthYear(999))))
+    }
+    #[test]
+    fn record_invalid_with_missing_fields() {
+        assert!(!Record(Default::default()).is_valid())
+    }
+    impl Record {}
+    fn make_minimal_record() -> Record {
+        Record(vec![
+            Field::BirthYear(0),
+            Field::IssueYear(0),
+            Field::ExpirationYear(0),
+            Field::Height(Length::Centimetre(0)),
+            Field::HairColour(Default::default()),
+            Field::EyeColour(Default::default()),
+            Field::PassportId(0),
+        ])
+    }
+    #[test]
+    fn record_valid_with_only_required_fields() {
+        assert!(make_minimal_record().is_valid())
+    }
+    #[test]
+    fn record_valid_with_all_fields() {
+        let mut r = make_minimal_record();
+        r.0.push(Field::CountryId(0));
+        assert!(r.is_valid())
+    }
+    #[test]
+    fn record_invalid_with_duplicate() {
+        let mut r = make_minimal_record();
+        r.0.push(Field::BirthYear(0));
+        assert!(!r.is_valid())
     }
 }
