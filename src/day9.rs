a9502819db6f762836a5fe8e2f3a99b5501972b9
jblam <jeremy.blamey@gmail.com>
2021-01-04 12:23:54 +1100

    Ugh

--- src/day9.rs
+++ src/day9.rs
@@ -14,20 +14,13 @@ pub fn part1(s: &str) -> Option<i64> {
         &mut buffer,
         s.lines()
             .skip(PREAMBLE_LENGTH)
-            .map(|l| l.parse::<i64>().unwrap())
+            .map(|l| l.parse::<i64>().unwrap()),
     )
 }
-pub fn part2(_: &str) {}
-
-fn build_buffer<T>(init: &[T]) -> CircularBuffer<T>
-where
-    T: Copy,
-{
-    let mut output = CircularBuffer::with_capacity(init.len());
-    for t in init {
-        output.push(*t);
-    }
-    output
+pub fn part2(s: &str) -> i64 {
+    let items = s.lines().map(|l| l.parse().unwrap());
+    let (min, max) = consume_p2(items, 25);
+    min + max
 }
 
 fn chain_tuples<'a, T>(slices: (&'a [T], &'a [T])) -> impl Iterator<Item = (&T, &T)> {
@@ -58,6 +51,44 @@ fn consume<T: Copy + PartialEq + Add<Output = T>>(
     }
     None
 }
+fn sums_to(vec: &[i64], i: i64) -> Option<&[i64]> {
+    for (idx, total) in vec
+        .iter()
+        .scan(0i64, |state, cur| {
+            *state += cur;
+            Some(*state)
+        })
+        .enumerate()
+    {
+        if total > i {
+            return None;
+        } else if total == i {
+            return Some(&vec[0..idx]);
+        }
+    }
+    None
+}
+
+fn consume_p2<'a>(input: impl Iterator<Item = i64> + 'a, preamble_length: usize) -> (i64, i64) {
+    let mut vec = Vec::<i64>::new();
+    fn is_valid(vec: &Vec<i64>, i: i64, preamble_length: usize) -> bool {
+        let (_, items) = vec.split_at(vec.len() - preamble_length);
+        tuples(items).any(|(&u, &v)| u + v == i)
+    };
+    for candidate in input {
+        if vec.len() > preamble_length && !is_valid(&vec, candidate, preamble_length) {
+            let range = (0..vec.len())
+                .map(|start| sums_to(&vec[start..], candidate))
+                .flatten()
+                .next()
+                .unwrap();
+            return (*range.iter().min().unwrap(), *range.iter().max().unwrap());
+        } else {
+            vec.push(candidate);
+        }
+    }
+    panic!()
+}
 
 #[cfg(test)]
 mod tests {
@@ -84,5 +115,18 @@ mod tests {
             Some(127i32),
             consume(&mut preamble, candidates.iter().cloned())
         );
+    }
+    #[test]
+    fn can_sum_to() {
+        let candidates = [15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182];
+        assert_eq!(Some([15, 25, 47, 40]), sums_to(&candidates, 127))
+    }
+    #[test]
+    fn can_find_sum() {
+        let candidates = [
+            35i64, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309,
+            576,
+        ];
+        assert_eq!([15, 25, 47, 40], consume_p2(candidates.iter().copied(), 5));
     }
 }
